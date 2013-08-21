b6e1c77f6202a64dac67690829549056100a7054
rlcmartis <rlcmartis@users.noreply.github.com>
2013-08-21 10:00:48 -0700

    Create frac_client.cpp given by the Prof. Arce

---
+++ frac_client.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "fracArray.h"
+ 
+int main() {
+    ArrayOfFractions L;
+ 
+    cout << "Contenido de L:" << endl;
+    L.print();
+ 
+    cout << "Maximo, minimo y suma de elementos en L" << endl;
+    L.max().print();
+    L.min().print();
+    L.sum().print(); 
+ 
+ 
+    L.sortAscending();
+    cout << "Contenido de L, luego de ordenado" << endl;
+    L.print();
+    
+    return 0;
+}
