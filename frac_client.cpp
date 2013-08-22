e12645900f6f6b62de08f30d3a669aa1c0cd241e
rlcmartis <rlcmartis@users.noreply.github.com>
2013-08-21 18:38:41 -0700

    Update frac_client.cpp
    Aunque se debía usar el cliente dado por el profesor se le añadió algunos "endl" para una mejor visualización.

--- frac_client.cpp
+++ frac_client.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 #include "fracArray.h"
- 
+using namespace std;
+
 int main() {
-    ArrayOfFractions L;
- 
-    cout << "Contenido de L:" << endl;
-    L.print();
- 
-    cout << "Maximo, minimo y suma de elementos en L" << endl;
-    L.max().print();
-    L.min().print();
-    L.sum().print(); 
- 
- 
-    L.sortAscending();
-    cout << "Contenido de L, luego de ordenado" << endl;
-    L.print();
-    
-    return 0;
+	ArrayOfFractions L;
+	system("clear");
+	cout << "Contenido de L:" << endl;
+	L.print();
+
+	cout << endl <<  "Maximo, minimo y suma de elementos en L" << endl;
+	L.maxi().print();
+	cout << endl;
+	L.mini().print();
+	cout << endl;
+	L.sum().print();
+	cout << endl;
+
+
+	L.sortAccending();
+	cout << "Contenido de L, luego de ordenado" << endl;
+	L.print();
+	cout << endl;
+	return 0;
 }
